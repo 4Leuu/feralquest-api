2652a376ca844b9fe5baff5241ed02a479e9b730
4Leuu <leonardo.mauricio404@gmail.com>
2024-10-03 17:15:23 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world');
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+app.get('/', (req, res) => {res.send("hello world")})
